d97a22182f370d15886e7613da038f7432ce4e8b
Emanuil <emanui_lnashev@abv.bg>
2022-04-06 18:10:40 +0300

    Added exercises for ConditionalStatementsAdvanced/Lab.

---
+++ Programming-Basics-Java/ConditionalStatementsAdvanced/Lab/WeekendOrWorkingDay.java
@@ -0,0 +1,31 @@
+package CanditionalStatementsAdvanced.Lab;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class WeekendOrWorkingDay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String dayOfWeek = scanner.nextLine();
+
+        switch (dayOfWeek){
+            case "Tuesday":
+            case "Monday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                System.out.println("Working day");
+                break;
+            case "Saturday":
+            case "Sunday":
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+
+
+        }
+    }
+}
