d3aaf8697b70e0b24665a5b900cfe985ec460235
Emanuil <emanui_lnashev@abv.bg>
2022-06-08 19:12:06 +0300

    Added an exercise for StacksAndQueues/Lab/DecimalToBinary.java

---
+++ JavaAdvanced/StacksAndQueues/Lab/DecimalToBinary.java
@@ -0,0 +1,27 @@
+package StacksAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int decimal = Integer.parseInt(scanner.nextLine());
+        if (decimal == 0){
+            System.out.println("0");
+            return;
+        }
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        while (decimal != 0) {
+            stack.push(decimal % 2);
+            decimal /= 2;
+        }
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            System.out.print(stack.pop());
+        }
+    }
+}
