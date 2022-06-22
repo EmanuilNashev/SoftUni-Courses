58c350898376c940d53adbfe730258ff3e221c5a
Emanuil <emanui_lnashev@abv.bg>
2022-06-22 19:55:58 +0300

    Added an exercise for SetsAndMapsAdvanced/Exercises/SetsOfElementWithArrays.java.

---
+++ JavaAdvanced/SetsAndMapsAdvanced/Exercises/SetsOfElementWithArrays.java
@@ -0,0 +1,15 @@
+package SetsAndMapsAdvanced.Exercises;
+
+public class SetsOfElementWithArrays {
+    public static void main(String[] args) {
+
+//        int[] arr = {1, 2, 3, 4, 5, 6};
+//        int[] arr2 = {2, 7, 8, 5, 9};
+//
+//        int maxSize = Math.max(arr.length, arr2.length);
+//
+//        for (int i = 0; i < maxSize; i++) {
+//            if (arr[maxSize - i])
+//        }
+    }
+}
