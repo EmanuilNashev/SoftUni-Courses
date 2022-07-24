11cefbad579b84d098b13a5a82f158bddbfdb97f
Emanuil <emanui_lnashev@abv.bg>
2022-07-24 15:27:49 +0300

    Added a folder and exercise for Encapsulation/Exercises/AnimalFarm.

---
+++ JavaOOP/Encapsulation/Exercises/AnimalFarm/Chicken.java
@@ -0,0 +1,44 @@
+package OOP.Encapsulation.Exercises.AnimalFarm;
+
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 15) {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+        this.age = age;
+    }
+
+    public double productPerDay() {
+        return calculateProductPerDay();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken Chichi (age %d) can produce %.2f eggs per day.", this.age, productPerDay());
+    }
+
+    private double calculateProductPerDay() {
+        if (this.age < 6) {
+            return 2;
+        } else if (this.age <= 11) {
+            return 1;
+        } else {
+            return 0.75;
+        }
+    }
+}
